7bfca937747349a2ca14e2936cedd80228a240cd
Lucas Santos <ilucas_@live.com>
2024-09-05 18:34:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "k8s_subnet_cidr" {
-  default = "172.31.48.0/20" // Altere esse valor para o correspondente com sua VPC
+  default = "172.31.96.0/20" // Altere esse valor para o correspondente com sua VPC
 }
 
 provider "aws" {
